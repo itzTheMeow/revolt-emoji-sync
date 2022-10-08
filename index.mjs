20216ed003b6cd1b669772b474a357dc6fbf3884
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-07 22:14:38 -0400

    auto create servers

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ import FormData from "form-data";
 
 const config = {
   root: "P:\\Media\\Images\\Emojis\\_lib", // folder to sync emojis from
-  serverID: "01GESPXKW68BW6WK0T636NTXK6",
+  nameTemplate: "MeowEmojis",
   emojiRegex: /^[a-z0-9_]+$/, // https://github.com/revoltchat/backend/blob/master/crates/delta/src/util/regex.rs
 };
 
@@ -16,33 +16,35 @@ const config = {
 
 const sessionData = JSON.parse(fs.readFileSync("session.json"));
 
-if (!fs.existsSync("emojis.json")) fs.writeFileSync("emojis.json", "{}");
-const emojidb = JSON.parse(fs.readFileSync("emojis.json"));
-const savedb = () => fs.writeFileSync("emojis.json", JSON.stringify(emojidb));
+if (!fs.existsSync("emojis.json")) fs.writeFileSync("emojis.json", `{"e":{},"s":[]}`);
+const saveddata = JSON.parse(fs.readFileSync("emojis.json"));
+const emojidb = saveddata.e;
+const serverList = saveddata.s;
+const savedb = () => fs.writeFileSync("emojis.json", JSON.stringify(saveddata));
 
 /* Example:
 {
-  "emojiName": {
-    "mod": "timestamp when the PNG was last modified",
-    "id": "id of the emoji"
-  }
+  "e": {
+    "emojiName": {
+      "mod": "timestamp when the PNG was last modified",
+      "id": "id of the emoji",
+      "serv": "server id"
+    }
+  },
+  "s": [array of server IDs]
 }
 */
 
 let scanning = false;
 let scanagain = false;
-/**
- *
- * @param {Server} server
- */
-async function scanEmojis(server) {
+async function scanEmojis() {
   if (scanning) return;
   console.log("Scanning emojis...");
   scanning = true;
   scanagain = false;
   const toAdd = fs.readdirSync(config.root);
   const hasEmojis = [...client.emojis.values()].filter(
-    (e) => e.parent.type == "Server" && e.parent.id == server._id
+    (e) => e.parent.type == "Server" && serverList.includes(e.parent.id)
   );
   await Promise.all(
     hasEmojis.map(async (e) => {
@@ -107,7 +109,42 @@ async function scanEmojis(server) {
       continue;
     }
     // this bucket gets ratelimited to 20 every 10s - so just wait a bit over the half second limit
-    await new Promise((r) => setTimeout(() => r(), 1000));
+    // should also work for the deletions
+    await new Promise((r) => setTimeout(() => r(), 750));
+    const server = await (async () => {
+      // searches servers to find the first one thats not full, otherwise will create one
+      const get = async (i) => {
+        const s = client.servers.get(serverList[i]);
+        if (s) {
+          if (hasEmojis.filter((e) => e.parent.id == s._id).length >= 99) return await get(i + 1);
+          else return s;
+        } else {
+          console.log(`Creating new server as the ${i} others are full.`);
+          const ns = await client.servers.createServer({ name: `${config.nameTemplate}${i + 1}` });
+          const form = new FormData();
+          form.append("file", png, `${f}.${ext.toLowerCase()}`);
+          // set the server icon to the emoji causing its creation
+          axios
+            .post("https://autumn.revolt.chat/icons", form, {
+              headers: form.getHeaders(),
+              data: form,
+            })
+            .then((res) =>
+              ns
+                .edit({
+                  icon: res.data.id,
+                })
+                .catch(console.error)
+            )
+            .catch(console.error);
+          ns.channels[0].delete().catch(console.error);
+          serverList.push(ns._id);
+          savedb();
+          return ns;
+        }
+      };
+      return await get(0);
+    })();
     const newEmoji = await client.api
       .put(`/custom/emoji/${fileID}`, {
         name: f,
@@ -124,6 +161,7 @@ async function scanEmojis(server) {
     emojidb[f] = {
       id: newEmoji._id,
       mod: createdAt,
+      serv: server._id,
     };
     savedb();
     console.log(`Created :${f}:.`);
@@ -138,12 +176,10 @@ const client = new Client({ autoReconnect: true });
 client.on("ready", async () => {
   console.log(`Client is now online as ${client.user.username}.`);
 
-  const server = await client.servers.fetch(config.serverID);
-  if (server) scanEmojis(server);
-  else console.error("Server not found.");
-  setInterval(() => scanEmojis(server), 1000 * 60 * 15); // scan every 15m too
+  scanEmojis();
+  setInterval(() => scanEmojis(), 1000 * 60 * 15); // scan every 15m too
   // sort of half works but ig itll do
-  fs.watch(config.root, {}, () => scanEmojis(server));
+  fs.watch(config.root, {}, () => scanEmojis());
 });
 client.on("emoji/delete", (id) => {
   client.emojis.delete(id);
